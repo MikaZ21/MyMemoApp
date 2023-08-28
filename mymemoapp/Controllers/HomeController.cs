7d3893c665607dff8525d95059f44d1be2bd6239
MikaZ21 <zukeyamamika@gmail.com>
2023-08-28 08:15:39 -0500

    Cleaned up indent HomeController

--- mymemoapp/Controllers/HomeController.cs
+++ mymemoapp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Common;
 using System.Diagnostics;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
 using mymemoapp.Models;
@@ -62,110 +63,111 @@ namespace mymemoapp.Controllers
 
 
 
-    internal MemoViewModel GetAllMemos()
-    {
-        List<MyMemo> memoList = new();
 
-        using (SqliteConnection con = 
-               new SqliteConnection("Data Source=db.sqlite"))
+        internal MemoViewModel GetAllMemos()
         {
-            using (var tableCmd = con.CreateCommand())
-            {
-                con.Open();
-                tableCmd.CommandText = "SELECT * FROM mymemo";
+            List<MyMemo> memoList = new();
 
-                using (var reader = tableCmd.ExecuteReader())
-                {
-                    if (reader.HasRows)
-                    {
-                        while (reader.Read())
-                        {
-                            memoList.Add(
-                                new MyMemo
-                                {
-                                    Id = reader.GetInt32(0),
-                                    Name = reader.GetString(1)
-                                });
-                        }
-                    }
-                    else
-                    {
-                        return new MemoViewModel
-                        {
-                            MemoList = memoList
-                        };
-                     
-                    }
-                };
-            }
-        }
-        return new MemoViewModel
-        {
-            MemoList = memoList
-        };
-    }
-
-    public RedirectResult Insert(MyMemo memo)
-    {
-        using (SqliteConnection con = 
-               new SqliteConnection("Data Source=db.sqlite"))
-        {
-            using (var tableCmd = con.CreateCommand())
-            {
-                con.Open();
-                tableCmd.CommandText = $"INSERT INTO mymemo (name) VALUES ('{memo.Name}')";
-                try
-                {
-                  tableCmd.ExecuteNonQuery();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-        }
-        return Redirect("https://localhost:7079/");
-    }
-
-    [HttpPost]
-    public JsonResult Delete(int id)
-    {
-        using (SqliteConnection con = 
-               new SqliteConnection("Data Source=db.sqlite"))
-        {
-            using (var tableCmd = con.CreateCommand())
-            {
-                con.Open();
-                tableCmd.CommandText = $"DELETE from mymemo WHERE Id = '{id}'";
-                tableCmd.ExecuteNonQuery();
-            }
-        }
-        return Json(new object{});
-    }
-
-    public RedirectResult Update(MyMemo memo)
-    {
             using (SqliteConnection con = 
-                  new SqliteConnection("Data Source=db.sqlite"))
-        {
-            using (var tableCmd = con.CreateCommand())
+                new SqliteConnection("Data Source=db.sqlite"))
             {
-                con.Open();
-                tableCmd.CommandText = $"UPDATE mymemo SET name = '{memo.Name}' WHERE Id = '{memo.Id}'";
-                try
+                using (var tableCmd = con.CreateCommand())
                 {
+                    con.Open();
+                    tableCmd.CommandText = "SELECT * FROM mymemo";
+
+                    using (var reader = tableCmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                memoList.Add(
+                                    new MyMemo
+                                    {
+                                        Id = reader.GetInt32(0),
+                                        Name = reader.GetString(1)
+                                    });
+                            }
+                        }
+                        else
+                        {
+                            return new MemoViewModel
+                            {
+                                MemoList = memoList
+                            };
+                        
+                        }
+                    };
+                }
+            }
+            return new MemoViewModel
+            {
+                MemoList = memoList
+            };
+        }
+
+        public RedirectResult Insert(MyMemo memo)
+        {
+            using (SqliteConnection con = 
+                new SqliteConnection("Data Source=db.sqlite"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = $"INSERT INTO mymemo (name) VALUES ('{memo.Name}')";
+                    try
+                    {
+                    tableCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            return Redirect("https://localhost:7079/");
+        }
+
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            using (SqliteConnection con = 
+                new SqliteConnection("Data Source=db.sqlite"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = $"DELETE from mymemo WHERE Id = '{id}'";
                     tableCmd.ExecuteNonQuery();
                 }
-                catch ( Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
             }
+            return Json(new object{});
         }
 
-        return Redirect("https://localhost:7079/");
+        public RedirectResult Update(MyMemo memo)
+        {
+                using (SqliteConnection con = 
+                    new SqliteConnection("Data Source=db.sqlite"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = $"UPDATE mymemo SET name = '{memo.Name}' WHERE Id = '{memo.Id}'";
+                    try
+                    {
+                        tableCmd.ExecuteNonQuery();
+                    }
+                    catch ( Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
 
+            return Redirect("https://localhost:7079/");
+
+        }
     }
-}
 }
 
